51603fbe78b8c28b07d5368845daceaceb699bf2
bodaghialib4 <bodaghialib4@gmail.com>
2019-12-03 21:06:28 +0330

    add Provider in index.js to access store in app as props

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from "react-dom";
 
 import TodoApp from "./TodoApp";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<TodoApp />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <TodoApp />
+    </Provider>,
+    rootElement
+);
