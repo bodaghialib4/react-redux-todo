08020edc121b76933efe2c6547daaccc21146ad0
bodaghialib4 <bodaghialib4@gmail.com>
2019-12-03 22:27:30 +0330

    connect TodoList.js component to redux state and correct selector.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Todo from "./Todo";
+import { connect } from "react-redux";
+import { getTodos,getTodosByVisibilityFilter } from "../redux/selectors";
+
+
 
 const TodoList = ({ todos }) => (
     <ul className="todo-list">
@@ -10,5 +14,33 @@ const TodoList = ({ todos }) => (
             : "No todos, yay!"}
     </ul>
 );
+/*
+const mapStateToProps = state => {
+    console.log(JSON.stringify(state));
+    const { byIds, allIds } = state.todos || {};
+    const todos =
+        allIds && allIds.length
+            ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
+            : null;
+    console.log(JSON.stringify(todos));
 
-export default TodoList;
+    return { todos };
+};
+
+export default connect(mapStateToProps)(TodoList);
+
+
+const mapStateToProps = state => {
+    const { visibilityFilter } = state
+    const todos = getTodosByVisibilityFilter(state, visibilityFilter)
+    return { todos }
+}
+
+export default connect(mapStateToProps)(TodoList)
+
+
+export default connect(state => ({ todos: getTodos(state) }))(TodoList);
+*/
+
+export default connect(state => ({ todos: getTodosByVisibilityFilter(state, state.visibilityFilter) }))(TodoList);
+
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,14 +1,10 @@
+export const getTodosState = store => store.todos;
+
 export const getTodoList = store =>
-    store && store.todos ? store.todos.allIds : [];
+    getTodosState(store) ? getTodosState(store).allIds : [];
 
 export const getTodoById = (store, id) =>
-    store && store.todos && store.todos.byIds
-        ? { ...store.todos.byIds[id], id }
-        : {};
+    getTodosState(store) ? { ...getTodosState(store).byIds[id], id } : {};
 
-/**
- * example of a slightly more complex selector
- * select from store combining information from multiple reducers
- */
 export const getTodos = store =>
     getTodoList(store).map(id => getTodoById(store, id));
